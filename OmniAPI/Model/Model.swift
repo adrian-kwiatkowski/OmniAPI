6948213cd3e5c59e90af464e2e865695f0f8879c
Adrian Kwiatkowski <kwiatkowski.adrian@icloud.com>
2020-02-17 22:58:39 +0100

    file structore

--- OmniAPI/Model/Model.swift
+++ OmniAPI/Model/Model.swift
@@ -412,6 +412,10 @@ class JSONNull: Codable, Hashable {
     public var hashValue: Int {
         return 0
     }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(0)
+    }
 
     public init() {}
 
