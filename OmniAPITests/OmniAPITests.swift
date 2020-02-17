f9f65bb9d6874c1c35602cf1eb8bd44cce5a23d4
Adrian Kwiatkowski <kwiatkowski.adrian@icloud.com>
2020-02-17 18:57:29 +0100

    Initial Commit

---
+++ OmniAPITests/OmniAPITests.swift
@@ -0,0 +1,34 @@
+//
+//  OmniAPITests.swift
+//  OmniAPITests
+//
+//  Created by Adrian Kwiatkowski 2 on 17/02/2020.
+//  Copyright © 2020 Adrian Kwiatkowski. All rights reserved.
+//
+
+import XCTest
+@testable import OmniAPI
+
+class OmniAPITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
